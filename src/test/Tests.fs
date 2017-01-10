413319579acc7ad354856ba6b818def00577020d
teves-castro <vcastro@equalexperts.com>
2017-01-10 10:31:55 +0000

    Test cleanup

--- src/test/Tests.fs
+++ src/test/Tests.fs
@@ -8,17 +8,9 @@ open DynamicProgramming
 
 [<Tests>]
 let tests =
-  testList "samples" [
-    testCase "universe exists" <| fun _ ->
-      let subject = true
-      Expect.isTrue subject "I compute, therefore I am."
+  testList "Hacker ranks test" [
 
     testCase "Maximum Sub Array" <| fun _ ->
       let subject = [| "3"; "4"; "1 2 3 4"; "6"; "2 -3 2 3 4 -5"; "4"; "-1 2 -3 -4"|] |> hacker maximumSubArray
       Expect.containsAll subject [|"10 10"; "9 11"; "2 2"; ""|] "Failed"
-
-    testCase "should fail" <| fun _ ->
-      let subject = false
-      Expect.isTrue subject "I should fail because the subject is false."
-
   ]
