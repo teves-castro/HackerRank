748706de2f335d7c9c117e4aca00da861fd6a87c
Vítor Castro <vcastro70@gmail.com>
2017-01-18 00:05:38 +0000

    Add units demo

---
+++ src/test/p2p/Units.fsx
@@ -0,0 +1,24 @@
+open System
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+
+let sq<[<Measure>] 'u>(x: float<'u>) = x * x
+
+let G = 6.67408e-11<m^3/kg/s^2>
+
+let earthMass = 5.97219e24<kg>
+
+let moonMass = 7.34767309e22<kg> //80.0<kg>
+
+let earthRadius = 6371000.0<m>
+let moonRadius = 1737000.0<m>
+
+let r = earthRadius + moonRadius
+
+let Fem = G * earthMass * moonMass / sq r
+
+let gEarth = G * earthMass / (sq earthRadius)
+let gMoon = G * moonMass / (sq moonRadius)
+
+let aEarth = Fg / earthMass
+
+let d = 0.5 * aEarth * 1.0<s^2>
