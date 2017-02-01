30c167193c10ba95dcf787a48ea6b891ca6956ea
Vítor Castro <vcastro70@gmail.com>
2017-02-01 21:42:12 +0000

    .

--- src/app/p2p/MaybeMonad.fs
+++ src/app/p2p/MaybeMonad.fs
@@ -18,3 +18,12 @@ let maybe = MaybeBuilder()
 let runM = function
     | Failure -> None
     | Success x -> Some x
+
+module List =
+
+    // The apply function for lists
+    // [f;g] apply [x;y] becomes [f x; f y; g x; g y]
+    let apply (fList: ('a->'b) list) (xList: 'a list)  = 
+        [ for f in fList do
+          for x in xList do
+              yield f x ]
